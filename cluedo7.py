c9fc5593214b62e81655eef3ca9599bd0abe43bf
dluke-oc <117632474+dluke-oc@users.noreply.github.com>
2023-01-23 09:25:27 +0100

    Update and rename cluedo6.py to cluedo7.py

---
+++ cluedo7.py
@@ -0,0 +1,38 @@
+import pyxel
+
+# taille de la fenetre 512x512 pixels
+# ne pas modifier
+pyxel.init(512, 512)
+mouse(True)
+
+x = 256
+y = 256
+
+# backgrounds
+
+    
+def update():
+# flèches interactives
+    global x,y
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()   
+    elif pyxel.btnp(pyxel.KEY_T):
+        y = y-10
+    elif pyxel.btnp(pyxel.KEY_G):
+        y = y+10
+    elif pyxel.btnp(pyxel.KEY_F):
+        x = x-10
+    elif pyxel.btnp(pyxel.KEY_H):
+        x = x+10
+     
+        
+# draw
+def draw():
+    """création des objets (30 fois par seconde)"""
+    global x,y
+    # vide la fenetre
+    pyxel.cls(0)
+    
+    pyxel.rect(x,y, 25,25, 13)
+    
+pyxel.run(update,draw)
