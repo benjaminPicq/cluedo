3b6656201a5015043080251d7347e3fddeb7e5f2
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2023-02-06 08:51:18 +0100

    Update and rename dodge3.py to dodge4.py

--- dodge4.py
+++ dodge4.py
@@ -1,14 +1,14 @@
 import pyxel, random
 
-# taille de la fenetre 512x512 pixels
+# taille de la fenetre 256x256 pixels
 # ne pas modifier
-pyxel.init(512, 512)
+pyxel.init(256, 256)
 """pyxel.load("images,.pyxres")"""
 
-x = 249
-y = 400
-w = 16
-h = 32
+x = 124
+y = 200
+w = 8
+h = 16
 ennemis_liste_up = []
 ennemis_liste_left = []
 score = 0
@@ -16,17 +16,17 @@ score = 0
 def personnage_deplacement(x, y):
     """déplacement avec les touches de directions"""
     if pyxel.btn(pyxel.KEY_RIGHT):
-        if (x < 512-w):
-            x = x+4
+        if (x < 256-w):
+            x = x+2
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 0):
-            x = x-4
+            x = x-2
     if pyxel.btn(pyxel.KEY_UP):
         if (y > 0):
-            y = y-4
+            y = y-2
     if pyxel.btn(pyxel.KEY_DOWN):
-        if (y < 512-h):
-            y = y+4
+        if (y < 256-h):
+            y = y+2
 
     return x, y
 
@@ -35,16 +35,16 @@ def ennemis_creation(ennemis_liste, direction): #direction 0 = left et direction
     # un ennemi par seconde
     if (pyxel.frame_count % 30 == 0):
         if direction == 1:
-            ennemis_liste.append([random.randint(0, 480), 0])
+            ennemis_liste.append([random.randint(0, 240), 0])
         else:
-            ennemis_liste.append([0, random.randint(0, 480)])
+            ennemis_liste.append([0, random.randint(0, 240)])
     return ennemis_liste
 
 def ennemis_deplacement(ennemis_liste, direction):
     """déplacement des ennemis vers le haut et suppression s'ils sortent du cadre"""
     for ennemi in ennemis_liste:
-        ennemi[direction] += 6
-        if  ennemi[direction]>512:
+        ennemi[direction] += 3
+        if  ennemi[direction]>256:
             ennemis_liste.remove(ennemi)
     return ennemis_liste
     
@@ -74,17 +74,17 @@ def draw():
     global x,y,ennemis_liste,ennemis_liste_left,score
     # vide la fenetre
     pyxel.cls(0)
-    pyxel.rect(0,0,512,512,7)     
+    pyxel.rect(0,0,256,256,7)     
 
   
     # dessiner le reste:
-    pyxel.text(350, 400, f"score: {score}", 6)
+    pyxel.text(175, 200, f"score: {score}", 6)
     # backgrounds
 
     pyxel.rect(x , y , w , h, 8)
     for ennemi in ennemis_liste_up:
-        pyxel.blt(ennemi[0], ennemi[1], 0, 0, 32, 32, 32)
+        pyxel.blt(ennemi[0], ennemi[1], 0, 0, 16, 16, 16)
     for ennemi in ennemis_liste_left:
-        pyxel.blt(ennemi[0], ennemi[1], 0, 0, 32, 32, 32)
+        pyxel.blt(ennemi[0], ennemi[1], 0, 0, 16, 16, 16)
     
 pyxel.run(update,draw)
