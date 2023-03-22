1ae18dbc8ca6c0a2e3a597e29134488a5f90b56a
charliefox06 <117631018+CharlieFox06@users.noreply.github.com>
2023-03-22 19:44:27 +0100

    Update and rename dodge18.py to dodge19.py

--- dodge19.py
+++ dodge19.py
@@ -1,6 +1,5 @@
 import pyxel, random
 #www.pyxelstudio.net/qwklx584
-#https://www.pyxelstudio.net/studio/4mp2f96v
 
 # taille de la fenetre 256x256 pixels
 # ne pas modifier
@@ -21,11 +20,13 @@ score = 0
 vies = 3
 game = True
 transparent_colour = 7
-transparent_colour_hearts = 2
+transparent_colour_2 = 2
 niveau = 0
 coins_liste = []
 hearts_liste = []
-
+clouds_liste = []
+bomb_liste = []
+obstacles_liste = []
 
 
 def score_timer(score):
@@ -105,21 +106,38 @@ def ennemis_deplacement(ennemis_liste, direction, sens): # sens 0 = left/up et s
 def collisions_ennemis(ennemis_liste, vies, sens):#sens 1 = left; sens 2 = right; sens 3 = up; sens 4 = down
     for ennemi in ennemis_liste:
         if sens == 1:
-            if ((personnage_x + w) >= (ennemi[0] + w) >= personnage_x) and ((personnage_y + h + (w/2)) >= (ennemi[1] + (w/2)) >= personnage_y):
+            if ((personnage_x + w +(w/2)) >= (ennemi[0] + w +(w/2)) >= personnage_x +(w/2)) and ((personnage_y + h + (h/2) + (h/2)) >= (ennemi[1] + (h/2) + (h/2)) >= personnage_y + (h/2)):
                     ennemis_liste.remove(ennemi)
                     vies = vies -1
-        if sens == 2:
-            if ((personnage_x + w) >= ennemi[0] >= personnage_x) and ((personnage_y + h + (w/2)) >= (ennemi[1] + (w/2)) >= personnage_y):
+        elif sens == 2:
+            if ((personnage_x + w +(w/2)) >= ennemi[0] +(w/2) >= personnage_x +(w/2)) and ((personnage_y + h + (h/2) + (h/2)) >= (ennemi[1] + (h/2) + (h/2)) >= personnage_y + (h/2)):
                     ennemis_liste.remove(ennemi)
                     vies = vies -1
-        if sens == 3:
-            if ((personnage_x + w + (w/2)) >= ennemi[0] + (w/2) >= personnage_x) and ((personnage_y + h) >= (ennemi[1] + h) >= personnage_y):
+        elif sens == 3:
+            if ((personnage_x + w + (w/2) +(w/2)) >= ennemi[0] + (w/2) +(w/2) >= personnage_x +(w/2)) and ((personnage_y + h + (h/2)) >= (ennemi[1] + h + (h/2)) >= personnage_y + (h/2)):
                     ennemis_liste.remove(ennemi)
                     vies = vies -1
-        if sens == 4:
-            if ((personnage_x + w + (w/2)) >= (ennemi[0] + (w/2)) >= personnage_x) and ((personnage_y + h) >= ennemi[1] >= personnage_y):
+        elif sens == 4:
+            if ((personnage_x + w + (w/2) +(w/2)) >= (ennemi[0] + (w/2) +(w/2)) >= personnage_x +(w/2)) and ((personnage_y + h + (h/2)) >= ennemi[1] + (h/2) >= personnage_y + (h/2)):
                     ennemis_liste.remove(ennemi)
                     vies = vies -1
+              
+              
+        for obstacle in obstacles_liste:
+            obst1 = obstacle[0] + w +(w/2)
+            if sens == 1:
+                if obst1 >= (ennemi[0] + w +(w/2)) >= (obst1-w) and ((obstacle[1] + h + (h/2) + (h/2)) >= (ennemi[1] + (h/2) + (h/2)) >= obstacle[1] + (h/2)):
+                        ennemis_liste.remove(ennemi)
+            elif sens == 2:
+                if ((obstacle[0] + w +(w/2)) >= ennemi[0] +(w/2) >= obstacle[0] +(w/2)) and ((obstacle[1] + h + (h/2) + (h/2)) >= (ennemi[1] + (h/2) + (h/2)) >= obstacle[1] + (h/2)):
+                        ennemis_liste.remove(ennemi)
+            elif sens == 3:
+                if ((obstacle[0] + w + (w/2) +(w/2)) >= ennemi[0] + (w/2) +(w/2) >= obstacle[0] +(w/2)) and ((obstacle[1] + h + (h/2)) >= (ennemi[1] + h + (h/2)) >= obstacle[1] + (h/2)):
+                        ennemis_liste.remove(ennemi)
+            elif sens == 4:
+                if ((obstacle[0] + w + (w/2) +(w/2)) >= (ennemi[0] + (w/2) +(w/2)) >= obstacle[0] +(w/2)) and ((obstacle[1] + h + (h/2)) >= ennemi[1] + (h/2) >= obstacle[1] + (h/2)):
+                        ennemis_liste.remove(ennemi)
+                        
     return ennemis_liste, vies
 
 
@@ -150,7 +168,45 @@ def hearts_collisions(hearts_liste, vies):
             if vies < 3:
                 vies += 1
     return hearts_liste, vies
+    
+    
+    
+    
+def obstacles_creation(obstacles_liste):
+    if len(obstacles_liste) < 2 :
+        if (pyxel.frame_count % 30 == 0):
+            obstacles_liste.append([random.randint(40,200), random.randint(40,200)])
+    return obstacles_liste
 
+
+
+def bomb_creation(bomb_liste):
+    if niveau >= 2:
+        if len(bomb_liste) < 1 :
+            if (pyxel.frame_count % 600 == 0):
+                bomb_liste.append([random.randint(40,200), random.randint(40,200)])
+    return bomb_liste
+
+def bomb_collisions(bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right):
+    for bombs in bomb_liste:
+        if ((personnage_x + w) >= bombs[0] >= personnage_x - w) and ((personnage_y + h) >= bombs[1] >= personnage_y - h):
+            bomb_liste.remove(bombs)
+            ennemis_liste_up = []
+            ennemis_liste_left = []
+            ennemis_liste_down = []
+            ennemis_liste_right = []
+            
+    return bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right
+
+def clouds_creation(clouds_liste):
+    if (pyxel.frame_count % 10 == 0):
+        clouds_liste.append([-w, random.randint(0, 240)])
+    return clouds_liste
+    
+def clouds_deplacement(clouds_liste):
+    for cloud in clouds_liste:
+        cloud[0] += 1
+    return clouds_liste
 
 
 # =========================================================
@@ -158,10 +214,12 @@ def hearts_collisions(hearts_liste, vies):
 # =========================================================
 def update():
 # flèches interactives
-    global personnage_x,personnage_y,ennemis_liste_up,ennemis_liste_left,ennemis_liste_down,ennemis_liste_right,score,game,vies,niveau, coins_liste, hearts_liste
+    global personnage_x,personnage_y,ennemis_liste_up,ennemis_liste_left,ennemis_liste_down,ennemis_liste_right,score,game,vies,niveau, coins_liste, hearts_liste, clouds_liste, bomb_liste, obstacles_liste
     if niveau == 0:
-        if pyxel.btn(pyxel.KEY_N):
+        if pyxel.btn(pyxel.KEY_S):
             niveau = 1
+        clouds_liste = clouds_creation(clouds_liste)
+        clouds_liste = clouds_deplacement(clouds_liste)
     if niveau >= 0:
         personnage_x, personnage_y = personnage_deplacement(personnage_x, personnage_y)
     if niveau >= 1:
@@ -180,19 +238,30 @@ def update():
         coins_liste, score = coins_collisions(coins_liste, score)
         hearts_liste = hearts_creation(hearts_liste)
         hearts_liste, vies = hearts_collisions(hearts_liste, vies)
+        bomb_liste = bomb_creation(bomb_liste)
+        bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right = bomb_collisions(bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right)
+        obstacles_liste = obstacles_creation(obstacles_liste)
+        hearts_liste, vies = hearts_collisions(hearts_liste, vies)
+        
         if niveau == 2:
             ennemis_liste_down = ennemis_creation(ennemis_liste_down, 1, 1)
             ennemis_liste_right = ennemis_creation(ennemis_liste_right, 0, 1)
             ennemis_liste_down = ennemis_deplacement(ennemis_liste_down, 1, 1)
             ennemis_liste_right = ennemis_deplacement(ennemis_liste_right, 0, 1)
+            bomb_liste = bomb_creation(bomb_liste)
+            bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right = bomb_collisions(bomb_liste, ennemis_liste_up, ennemis_liste_left, ennemis_liste_down, ennemis_liste_right)
         if pyxel.btn(pyxel.KEY_SPACE):
             game = False
             vies = 0
             ennemis_liste_up = []
             ennemis_liste_left = []
+            coins_liste = []
+            hearts_liste = []
+            obstacles_liste = []
             if niveau == 2:
                 ennemis_liste_down = []
                 ennemis_liste_right = []
+                bomb_liste = []
         if pyxel.btn(pyxel.KEY_R):
             game = True
             vies = 3
@@ -201,31 +270,36 @@ def update():
             personnage_y = 120
             ennemis_liste_up = []
             ennemis_liste_left = []
+            coins_liste = []
+            hearts_liste = []
+            obstacles_liste = []
             if niveau == 2:
                 ennemis_liste_down = []
                 ennemis_liste_right = []
-            niveau = 1
+                bomb_liste = []
+            niveau = 0
 # =========================================================
 # == DRAW
 # =========================================================
 def draw():
     """création des objets (30 fois par seconde)"""
-    global personnage_x,personnage_y,ennemis_liste_up,ennemis_liste_left,ennemis_liste_down,ennemis_liste_right,score,vies,coins_liste
+    global personnage_x,personnage_y,ennemis_liste_up,ennemis_liste_left,ennemis_liste_down,ennemis_liste_right,score,vies,coins_liste, clouds_liste, transparent_colour, bomb_liste, obstacles_liste
     # vide la fenetre
     pyxel.cls(0)
     if niveau == 0:
-        pyxel.bltm(0,0,0,256,0,255,255)
-        pyxel.blt(personnage_x, personnage_y, 0, 0, 0, 16, 16, transparent_colour)
+        pyxel.bltm(0, 0, 0, 512, 0, 256, 256)
+        for cloud in clouds_liste:
+            pyxel.blt(cloud[0], cloud[1], 0, 0, 64, 16, 8, transparent_colour_2)
+        pyxel.bltm(0, 0, 0, 256, 0, 256, 256, transparent_colour)
+        pyxel.blt(personnage_x, personnage_y, 0, 32, 64, 16, 19, transparent_colour_2)
+        pyxel.text(95, 210, "Press 'S' to Start", 0)
     if niveau >= 1:
         pyxel.rect(0, 0, 255, 255, 7)
     
         # backgrounds
-        pyxel.bltm(0,0,0,0,0,255,255)
-        
+        pyxel.bltm(0, 0, 0, 0, 0, 255, 255)
         pyxel.text(175, 190, f"Niveau: {niveau}", 0)
         
-
-      
         # dessiner le reste:
         if niveau == 1:
             pyxel.text(175, 200, f"Score: {score}/100", 0)
@@ -235,37 +309,44 @@ def draw():
         pyxel.blt(personnage_x, personnage_y, 0, 0, 0, 16, 16, transparent_colour)
         
         for ennemi in ennemis_liste_up:
-            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48, 8, 16, transparent_colour)
+            pyxel.blt(ennemi[0], ennemi[1], 0, 16, 80, 8, -16, transparent_colour)
         for ennemi in ennemis_liste_left:
-            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48, 16, 8, transparent_colour)
+            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48 + 8, 16, 8, transparent_colour)
         for ennemi in ennemis_liste_down:
-            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48, 8, 16, transparent_colour)
+            pyxel.blt(ennemi[0], ennemi[1], 0, 16, 80, 8, 16, transparent_colour)
         for ennemi in ennemis_liste_right:
-            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48, 16, 8, transparent_colour)
+            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 48 + 8, -16, 8, transparent_colour)
+            
+        for bombs in bomb_liste:
+            pyxel.blt(bombs[0], bombs[1], 0, 0, 80, 16, 16, transparent_colour_2)
             
         for coin in coins_liste:
             pyxel.blt(coin[0], coin[1], 0, 0, 32, 16, 16, transparent_colour)
             
         for heart in hearts_liste:
-            pyxel.blt(heart[0], heart[1], 0, 0, 16, 16, 16, transparent_colour_hearts)
+            pyxel.blt(heart[0], heart[1], 0, 0, 16, 16, 16, transparent_colour_2)
+            
+        for obstacle in obstacles_liste:
+            pyxel.blt(obstacle[0], obstacle[1], 0, 48, 64, 16, 16)
         
         if game == False:
             pyxel.cls(7)
+            pyxel.bltm(0, 0, 0, 512, 0, 256, 256)
             pyxel.text(110, 100, "Game Over", 0)
             pyxel.text(90, 110, "Press 'R' to Restart", 0)
             pyxel.text(110, 120, f"Score: {score}", 0)
         
         if vies == 3:
-            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
-            pyxel.blt(24, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
-            pyxel.blt(44, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
+            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_2)
+            pyxel.blt(24, 4, 0, 0, 16, 16, 16, transparent_colour_2)
+            pyxel.blt(44, 4, 0, 0, 16, 16, 16, transparent_colour_2)
         elif vies == 2:
-            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
-            pyxel.blt(24, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
-            pyxel.blt(44, 4, 0, 32, 48, 16, 16, transparent_colour_hearts)
+            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_2)
+            pyxel.blt(24, 4, 0, 0, 16, 16, 16, transparent_colour_2)
+            pyxel.blt(44, 4, 0, 32, 48, 16, 16, transparent_colour_2)
         elif vies == 1:
-            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_hearts)
-            pyxel.blt(24, 4, 0, 32, 48, 16, 16, transparent_colour_hearts)
-            pyxel.blt(44, 4, 0, 32, 48, 16, 16, transparent_colour_hearts)
+            pyxel.blt(4, 4, 0, 0, 16, 16, 16, transparent_colour_2)
+            pyxel.blt(24, 4, 0, 32, 48, 16, 16, transparent_colour_2)
+            pyxel.blt(44, 4, 0, 32, 48, 16, 16, transparent_colour_2)
 
 pyxel.run(update,draw)
